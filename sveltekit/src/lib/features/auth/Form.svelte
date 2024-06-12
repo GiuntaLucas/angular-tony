0cce68a84174f66591d373b426ff318f06e2ddac
Lucas Giunta <lucas.giunta@belgiantrain.be>
2024-06-12 15:12:58 +0200

    create auth form component

---
+++ sveltekit/src/lib/features/auth/Form.svelte
@@ -0,0 +1,12 @@
+<form class="card w-96 bg-base-100 shadow-xl">
+  <div class="card-body">
+    <h2 class="card-title">Login</h2>
+    <div>
+      <input name="email" type="text" placeholder="Email" class="input w-full max-w-xs" />
+      <input name="password" type="password" placeholder="Password" class="input w-full max-w-xs" />
+    </div>
+    <div class="card-actions justify-end">
+      <button type="button" class="btn btn-outline btn-primary btn-md">Save</button>
+    </div>
+  </div>
+</form>
--- sveltekit/src/routes/+page.svelte
+++ sveltekit/src/routes/+page.svelte
@@ -1,1 +1,7 @@
+<script>
+  import Form from "$lib/features/auth/Form.svelte";
+</script>
 
+<div class="flex justify-center items-center">
+  <Form />
+</div>
